66be61b47f6be18caddf2cb789245fe79e146030
jeremy <jeremy>
2021-07-13 18:09:28 +1000

    Add weather icon and create a sanitize data function

--- src/components/ForecastCard.tsx
+++ src/components/ForecastCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const ForecastCard = ({ date, temp, weather }: IForecastCard) => (
+const ForecastCard = ({ date, temp, weather, iconId }: IForecastCard) => (
   <div>
     <h3>{date}</h3>
     <p>Temperature: {Math.round(temp)}</p>
     <p>Weather: {weather}</p>
+    <img src={`http://openweathermap.org/img/wn/${iconId}@2x.png`} alt={weather} />
   </div>
 )
 
@@ -12,6 +13,7 @@ interface IForecastCard {
   date: string
   temp: number
   weather: string
+  iconId: string
 }
 
 export default ForecastCard
--- src/components/ForecastResult.tsx
+++ src/components/ForecastResult.tsx
@@ -4,25 +4,31 @@ import React from 'react'
 import ForecastCard from './ForecastCard'
 
 const ForecastResult = ({ data }: ICurrentWeatherState) => {
+  const sanitizeData = (dt: number, temp: number, weather: { description: string; icon: string }[]) => ({
+    date: new Date(dt * 1000).toLocaleString(),
+    temp: Math.round(temp),
+    weather: weather[0].description,
+    iconId: weather[0].icon,
+  })
+
   if (!data) return null
 
   const { city, current, daily } = data
 
+  const currentSanitized = sanitizeData(current.dt, current.temp, current.weather)
+
   const next5DaysForecast = daily.slice(1, 6) //Here we keep only the 5 next days
+  const next5DaysForecastSanitized = next5DaysForecast.map((item) => sanitizeData(item.dt, item.temp.day, item.weather))
+
+  const forecastTimeline = [currentSanitized, ...next5DaysForecastSanitized]
 
   return (
     <>
       <h2>City: {city}</h2>
 
-      <ForecastCard date="Current time" temp={Math.round(current.temp)} weather={current.weather[0].description} />
-
-      {next5DaysForecast.map((item: any) => (
+      {forecastTimeline.map((item: any) => (
         <React.Fragment key={item.dt}>
-          <ForecastCard
-            date={new Date(item.dt * 1000).toLocaleString()}
-            temp={Math.round(item.temp.day)}
-            weather={item.weather[0].description}
-          />
+          <ForecastCard date={item.date} temp={item.temp} weather={item.weatherDescription} iconId={item.iconId} />
         </React.Fragment>
       ))}
     </>
@@ -33,12 +39,14 @@ interface ICurrentWeatherState {
   data: {
     city: string
     current: {
+      dt: number
       temp: number
-      weather: { description: string }[]
+      weather: { description: string; icon: string }[]
     }
     daily: {
+      dt: number
       temp: { day: number }
-      weather: { description: string }[]
+      weather: { description: string; icon: string }[]
     }[]
   } | null
 }
